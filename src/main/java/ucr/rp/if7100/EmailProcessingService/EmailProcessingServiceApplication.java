536549e979c0a55c35d025cc9166fa515d86ddc3
unknown <allcincoceroseis@gmail.com>
2023-05-31 15:18:59 -0600

    Liquid working
    Config for liquid

---
+++ src/main/java/ucr/rp/if7100/EmailProcessingService/EmailProcessingServiceApplication.java
@@ -0,0 +1,13 @@
+package ucr.rp.if7100.EmailProcessingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailProcessingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailProcessingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ucr/rp/if7100/EmailProcessingService/EmailProcessingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ucr.rp.if7100.EmailProcessingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailProcessingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
