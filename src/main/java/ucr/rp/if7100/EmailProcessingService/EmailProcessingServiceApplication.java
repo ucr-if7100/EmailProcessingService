d9705e36175ac42c10988ef69240348ca7d43b1c
Jeison Araya Mena <57369279+jeison-araya@users.noreply.github.com>
2023-06-08 17:08:54 -0600

    Eureka configura added

--- src/main/java/ucr/rp/if7100/EmailProcessingService/EmailProcessingServiceApplication.java
+++ src/main/java/ucr/rp/if7100/EmailProcessingService/EmailProcessingServiceApplication.java
@@ -2,8 +2,11 @@ package ucr.rp.if7100.EmailProcessingService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EmailProcessingServiceApplication {
 
 	public static void main(String[] args) {
