4b1d6c4d41787fe368f3228b96ae905f1886059c
LuisGa24 <69638645+LuisGa24@users.noreply.github.com>
2023-06-11 22:20:58 -0600

    Cambio del tipo de los ID, Implementación del patrón Buider.
    Se cambió el tipo de dato de Int a String. Intenté usar el tipo de java UUID, pero tuve problemas de compatibilidad con JPA y la BD. Aún así los ids en base de datos tiene el formato de UUID.

--- src/main/java/ucr/rp/if7100/EmailProcessingService/entities/Bank.java
+++ src/main/java/ucr/rp/if7100/EmailProcessingService/entities/Bank.java
@@ -1,30 +1,50 @@
 package ucr.rp.if7100.EmailProcessingService.entities;
 
 import javax.persistence.*;
+import java.util.UUID;
 
 @Entity
 @Table(name = "bank")
 public class Bank {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private String id;
 
     private String name;
 
-    public Long getId() {
-        return id;
+    protected Bank() {
+        // Constructor vacío para JPA
     }
 
-    public void setId(Long id) {
+    private Bank(String id, String name) {
         this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public static class Builder {
+        private String id;
+        private String name;
+
+        public Builder() {
+            // Generar un ID aleatorio utilizando UUID
+            this.id = UUID.randomUUID().toString();
+        }
+
+        public Builder withName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Bank build() {
+            return new Bank(id, name);
+        }
     }
 }
--- src/main/java/ucr/rp/if7100/EmailProcessingService/repositories/BankRepository.java
+++ src/main/java/ucr/rp/if7100/EmailProcessingService/repositories/BankRepository.java
@@ -3,5 +3,5 @@ package ucr.rp.if7100.EmailProcessingService.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ucr.rp.if7100.EmailProcessingService.entities.Bank;
 
-public interface BankRepository extends JpaRepository<Bank, Long> {
+public interface BankRepository extends JpaRepository<Bank, String> {
 }
