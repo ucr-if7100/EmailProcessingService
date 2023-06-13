2e85d47040d41726030d3931353f082ef5d2349a
Paola Vega <82129194+pavega@users.noreply.github.com>
2023-06-13 10:34:11 -0600

    Job para guardar información de los emails (#3)
    * Se agregó el job para guardar en la base de datos la información de los emails no leídos
    
    * Actualización del tipo para la variable content

--- src/main/java/ucr/rp/if7100/EmailProcessingService/EmailProcessingServiceApplication.java
+++ src/main/java/ucr/rp/if7100/EmailProcessingService/EmailProcessingServiceApplication.java
@@ -2,8 +2,10 @@ package ucr.rp.if7100.EmailProcessingService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class EmailProcessingServiceApplication {
 
 	public static void main(String[] args) {
