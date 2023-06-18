28d08f20f6682f76777717bdc2dc7fe09b503894
pavega <vega.vasquezpao@gmail.com>
2023-06-17 19:51:57 -0600

    Kafka Config

--- src/main/java/ucr/rp/if7100/EmailProcessingService/services/TransactionService.java
+++ src/main/java/ucr/rp/if7100/EmailProcessingService/services/TransactionService.java
@@ -1,20 +1,38 @@
 package ucr.rp.if7100.EmailProcessingService.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.event.EventListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ucr.rp.if7100.EmailProcessingService.entities.Transaction;
+import ucr.rp.if7100.EmailProcessingService.events.Event;
+import ucr.rp.if7100.EmailProcessingService.events.TransactionCreatedEvent;
 import ucr.rp.if7100.EmailProcessingService.repositories.TransactionRepository;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class TransactionService {
+
+    @Value("${kafka.topic}")
+    private String topic;
     private final TransactionRepository transactionRepository;
 
+    private final ApplicationEventPublisher eventPublisher;
+
+    private final KafkaTemplate<String, Event<?>> kafkaTemplate;
+
     @Autowired
-    public TransactionService(TransactionRepository transactionRepository) {
+    public TransactionService(TransactionRepository transactionRepository, ApplicationEventPublisher eventPublisher, KafkaTemplate<String, Event<?>> kafkaTemplate) {
         this.transactionRepository = transactionRepository;
+        this.eventPublisher = eventPublisher;
+        this.kafkaTemplate = kafkaTemplate;
     }
 
     public List<Transaction> getAllTransactions() {
@@ -33,5 +51,25 @@ public class TransactionService {
     //public void deleteTransaction(Long id) {
     public void deleteTransaction(String id) {
         transactionRepository.deleteById(id);
+    }
+
+
+    @Transactional
+    public Transaction createTransaction(Transaction transaction) {
+        Transaction createdTransaction= transactionRepository.save(transaction);
+        TransactionCreatedEvent transactionCreatedEvent = new TransactionCreatedEvent();
+        transactionCreatedEvent.setId(UUID.randomUUID().toString());
+        transactionCreatedEvent.setCorrelationalId(UUID.randomUUID().toString());
+        transactionCreatedEvent.setDate(new Date());
+        transactionCreatedEvent.setPayload(transaction);
+        eventPublisher.publishEvent(transactionCreatedEvent);
+        return createdTransaction;
+    }
+
+    @EventListener
+    public void handlePersonCreated(TransactionCreatedEvent event) {
+        TransactionCreatedEvent transactionCreatedEvent = event;
+        // Enviar el objeto Person a Kafka
+        kafkaTemplate.send(topic, transactionCreatedEvent);
     }
 }
